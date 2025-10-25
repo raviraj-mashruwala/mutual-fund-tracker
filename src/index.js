c1d84a51670fab440103421700ffbefa9915d828
Raviraj Mashruwala <mashruwala_raviraj@yahoo.com>
2025-10-26 00:02:05 +0530

    Browser Router Update

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/mutual-fund-tracker">
       <ThemeProvider>
         <AuthProvider>
           <Routes>
